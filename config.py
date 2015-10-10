3f0a1207df9e83fc2964f6a55110a2af437b74db
Nocternology <nocternology@gmail.com>
2015-10-10 12:53:33 +0200

    Modified : project folder structure. Add : Base html page, index page (overview), signin and logout. Add : Basic handeling of login/logout.

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration file for Fail2Dash
+
+# Basic configuration about the application
+debug = True
+
+# Authentication configuration
+auth_mail = "root@localhost"
+auth_password = "test"
