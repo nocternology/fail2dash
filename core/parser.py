170d08442aeabaa5f8e2fb1fef14f9e6464eeeab
Nocternology <nocternology@gmail.com>
2015-10-15 21:25:07 +0200

    Add : implemented parsing method.

--- core/parser.py
+++ core/parser.py
@@ -12,9 +12,34 @@ class Parser(object):
         self.config = config
         if mode is None:
             self.mode = config.parse_default
+            self.log = open(config.log_file)
+
+    def isLog(line):
+        """
+        Checks if a log entry is a fail2ban log entry
+        """
+        # TODO : Change this to some regex magic ?
+        if "fail2ban" in line and "Ban" in line:
+            return True
+        else:
+            return False
 
     def parseAll(self):
         """
         Parses all the previous entries in the fail2ban log without realtime
         support.
         """
+        entries = []
+        while True:
+            line = self.log.readline()
+            if self.isLog(line):
+                entries.append(line)
+
+        return entries
+
+    def parseRT(self):
+        """
+        Parses the log file in realtime : only the upcoming log bans will be
+        """
+        # TODO : maybe use a separate thread for rt parsing ?
+        pass
